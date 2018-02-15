4cb6865b9050cbeaca0ea4fd29945e0851f304e5
Papa Deere <papadeere5150@gmail.com>
2018-02-15 14:18:01 -0700

    v2

--- server.js
+++ server.js
@@ -23,6 +23,5 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
-});
+app.listen(port);
+  console.log("App now listening at localhost:" + port);
