b0943b21d6ac32b7e958acf5308207f15b7de0fd
Papa Deere <papadeere5150@gmail.com>
2018-02-15 14:16:33 -0700

    V2

--- config/orm.js
+++ config/orm.js
@@ -7,85 +7,85 @@ var connection = require("../config/connection.js");
 // The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
 // ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
-  var arr = [];
+    var arr = [];
 
-  for (var i = 0; i < num; i++) {
-    arr.push("?");
-  }
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
 
-  return arr.toString();
+    return arr.toString();
 }
 
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
-  var arr = [];
+    var arr = [];
 
-  // loop through the keys and push the key/value as a string int arr
-  for (var key in ob) {
-    arr.push(key + "=" + ob[key]);
-      }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
-      arr.push(key + "=" + value);
-   
-      // translate array of strings to a single comma-separated string
-      return arr.toString();
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        arr.push(key + "=" + ob[key]);
+    }
+    // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+    // e.g. {sleepy: true} => ["sleepy=true"]
+    arr.push(key + "=" + value);
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
 }
 
 // Object for all our SQL statement functions.
 var orm = {
-  selectAll: function(tableInput, cb) {
-    var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  },
-  insertOne: function(table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + table;
+    selectAll: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
 
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
 
-    console.log(queryString);
+        console.log(queryString);
 
-    connection.query(queryString, vals, function(err, result) {
-      if (err) {
-        throw err;
-      }
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
 
-      cb(result);
-    });
-  },
-  //Function that updates a single table entry
-  // An example of objColVals would be {name: cheeseburger, devoured: true}
-  updateOne: function(table, objColVals, condition, cb) {
-    //Construct the query string that updates the entry in the target table
-    var queryString = "UPDATE " + table;
+            cb(result);
+        });
+    },
+    //Function that updates a single table entry
+    // An example of objColVals would be {name: cheeseburger, devoured: true}
+    updateOne: function(table, objColVals, condition, cb) {
+        //Construct the query string that updates the entry in the target table
+        var queryString = "UPDATE " + table;
 
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
 
-    console.log(queryString);
+        console.log(queryString);
 
-    // Perform the database query
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
+        // Perform the database query
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
 
-      //Return results in callback
-      cb(result);
-    });
-  }
+            //Return results in callback
+            cb(result);
+        });
+    }
 };
 
 // Export the orm object for the model (burger.js).
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,6 +7,10 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
+    res.redirect("/index");
+});
+
+router.get("/index", function(req, res) {
     burger.selectAll(function(data) {
         var hbsObject = {
             burgers: data
